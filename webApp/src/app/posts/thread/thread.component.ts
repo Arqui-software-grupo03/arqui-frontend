6594ff7e03c860521178556452b426af90b82d63
Felipe De la Fuente V <fadelafuente1@gmail.com>
2018-10-21 00:34:58 -0300

    add thread structure

--- webApp/src/app/posts/thread/thread.component.ts
+++ webApp/src/app/posts/thread/thread.component.ts
@@ -7,8 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThreadComponent implements OnInit {
 
-  constructor() { }
-
+  userPhotoUrl;
+  topic;
+  response;
+  hour;
+  date;
+  message;
+  constructor() {
+    this.userPhotoUrl = '../../assets/chau.jpg';
+    this.topic = 'Topic 1';
+    this.response = 'Stephanie Chau';
+    this.hour = '17:38';
+    this.date = '7 septiembre';
+    this.message = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"
+                    + "dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen"
+                    + "book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially"
+                    + "unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more"
+                    + "recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  }
   ngOnInit() {
   }
 
