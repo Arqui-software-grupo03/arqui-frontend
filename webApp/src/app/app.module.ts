015bd244f677f0271415ffc70011cd10afd9c592
Stephanie Chau <schau@uc.cl>
2018-10-21 14:48:59 -0300

    Added HttpClientModule injection

--- webApp/src/app/app.module.ts
+++ webApp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
@@ -23,7 +24,8 @@ import { AppComponent } from './app.component';
     SidebarModule,
     UsersModule,
     AuthModule,
-    InfiniteScrollModule
+    InfiniteScrollModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [ AppComponent ]
