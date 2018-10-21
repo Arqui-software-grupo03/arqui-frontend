6eec2e5e5c533984b6e345aea0e39f900975383f
Stephanie Chau <schau@uc.cl>
2018-10-21 12:03:51 -0300

    Added tooltips and disabled btn until inputs added

--- webApp/src/app/navbar/login/login.component.ts
+++ webApp/src/app/navbar/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { LogInService } from './login.service';
+import * as $ from 'jquery';
+
+declare var jQuery: any;
 
 @Component({
   selector: 'app-login',
@@ -6,13 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LogInComponent implements OnInit {
-
-  constructor() { }
+  user;
+  btnEnable = false;
+  constructor(private logInService: LogInService ) { }
 
   ngOnInit() {
+    this.addjQueryTooltip();
+  }
+  addjQueryTooltip() {
+    jQuery('[data-toggle="tooltip"]').tooltip({
+      trigger: 'hover'
+    }).on('click', () => {
+      jQuery(jQuery('[data-toggle="tooltip"]')).tooltip('hide');
+    });
   }
   keyDownFunction(username, password, event) {
-
+    this.btnEnable = username.length > 4 && password.length > 6 ? true : false;
   }
   onSubmit(username, password, event) {
     event.preventDefault();
