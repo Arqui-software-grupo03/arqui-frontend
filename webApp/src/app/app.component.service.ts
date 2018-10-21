46efad06f4ebcc2bd59ce2b40049b81de027137d
Stephanie Chau <schau@uc.cl>
2018-10-21 14:49:35 -0300

    File created to share url

---
+++ webApp/src/app/app.component.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AppService {
+    httpOptions = {
+        headers: new HttpHeaders({'Content-Type': 'application/json'})
+    };
+    // Apiary: http://private-bb6ce2-arquitransocialnetwork.apiary-mock.com/
+    url = 'http://private-bb6ce2-arquitransocialnetwork.apiary-mock.com/';
+    constructor() { }
+}
