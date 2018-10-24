b20ed20c0be57ad7fee9c3a52952810d96fd0d41
Stephanie Chau <schau@uc.cl>
2018-10-24 14:42:53 -0300

    Check retype password after user is finished typing; Added close btn to flashMessage; After btn enables, changes color

--- webApp/src/app/navbar/signup/signup.component.ts
+++ webApp/src/app/navbar/signup/signup.component.ts
@@ -14,6 +14,8 @@ export class SignUpComponent implements OnInit {
   btnEnable = false;
   waitingResponse = false;
   validEmail = true;
+  private userTypingTimeout;
+  private showingMessage = false;
   constructor(private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
@@ -31,20 +33,47 @@ export class SignUpComponent implements OnInit {
   keyUpValidatePassword(password: string, retypePassword: string) {
     this.btnEnable = (password.length >= 6 && retypePassword.length >= 6) &&
                         (password === retypePassword) && this.validEmail ? true : false;
+    if (password.length >= 6 && retypePassword.length >= 1) {
+      clearTimeout(this.userTypingTimeout);
+      this.userTypingTimeout = this.checkAfterUserTypes(password, retypePassword);
+    }
   }
   keyUpCheckEmail(email: string) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     this.validEmail = re.test(String(email).toLowerCase());
   }
-  onSubmit(username: string, password: string, event) {
-
+  onSubmit(username: string, password: string, $event) {
+    $event.stopPropagation();
+    $event.preventDefault();
+    this.waitingResponse = true;
   }
 
   showMessage(message: string, type: string) {
     this.flashMessage.show(message, {
       cssClass: `alert-${type}`,
-      timeout: TIMEOUT
+      timeout: TIMEOUT,
+      showCloseBtn: true
     });
   }
 
+  checkAfterUserTypes(password: string, retypePassword: string) {
+    return setTimeout( () => {
+      if ((password !== retypePassword) && !this.showingMessage) {
+        this.showMessage('Las contraseñas ingresadas no coinciden.', 'danger');
+        this.changeShowingMessageState(true);
+      }
+    }, 500);
+  }
+
+  changeShowingMessageState(value: boolean): void {
+    this.showingMessage = value;
+    setTimeout( () => {
+      this.showingMessage = !this.showingMessage;
+    }, TIMEOUT);
+  }
+
+  close(event) {
+    event.stopPropagation();
+    event.preventDefault();
+  }
 }
