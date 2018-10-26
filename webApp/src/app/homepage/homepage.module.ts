604afb3d8e20538a43c2ac227dedb1e02a01dae8
Stephanie Chau <schau@uc.cl>
2018-10-26 14:46:53 -0300

    Added module

--- webApp/src/app/homepage/homepage.module.ts
+++ webApp/src/app/homepage/homepage.module.ts
@@ -1,11 +1,13 @@
 import { HomepageComponent } from '@app/homepage/homepage.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { LoginModule } from '@app/navbar/login/login.module';
 
 
 @NgModule({
     imports: [
         CommonModule,
+        LoginModule
     ],
     declarations: [
         HomepageComponent
