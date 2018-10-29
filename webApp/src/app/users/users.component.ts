14eb04084d7446e372c5411c87adeac07ecef6b1
Stephanie Chau <schau@uc.cl>
2018-10-29 20:03:04 -0300

    User profile template

--- webApp/src/app/users/users.component.ts
+++ webApp/src/app/users/users.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from './users.service';
+import * as $ from 'jquery';
+
+declare var jQuery: any;
 
 @Component({
   selector: 'app-users',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  user;
+  constructor(private usersService: UsersService) { }
 
   ngOnInit() {
+    this.usersService.castUser.subscribe(
+      usr => this.user = usr
+    );
+    setTimeout(
+      () => this.getData(), 50
+    );
+    this.addjQueryTooltip();
+  }
+
+  getData() {
+    this.user.followers = 10;
+    this.user.following = 20;
+    this.user.url = 'https://s17.postimg.cc/xcbukrwdr/Hugh_Jackman_f.jpg';
+    this.user.username = 'Name';
+  }
+
+  addjQueryTooltip() {
+    jQuery('[data-toggle="tooltip"]').tooltip({
+      trigger: 'hover'
+    }).on('click', () => {
+      jQuery(jQuery('[data-toggle="tooltip"]')).tooltip('hide');
+    });
   }
 
 }
