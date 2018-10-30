09ef2b73fc39e1f9d9de00ed3907f57ad681996d
Felipe De la Fuente V <fadelafuente1@gmail.com>
2018-10-30 18:31:03 -0300

    add onKeyDown and onSubmit event

--- webApp/src/app/posts/form/form.component.ts
+++ webApp/src/app/posts/form/form.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-
+  inputText: string;
   constructor() { }
 
   ngOnInit() {
+  }
+  onChangeInput(event) {
+    this.inputText = event.target.value;
+  }
+
+  onSubmit(event) {
+    console.log(this.inputText);
+  }
+  onKeyDown(event) {
+    this.onChangeInput(event);
+    if (event.keyCode === 13) {
+      this.onSubmit(event);
+    }
   }
 
 }
