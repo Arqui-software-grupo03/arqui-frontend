42d6bc774d2fece72391ef20f104004a8e0f6a88
Felipe De la Fuente V <fadelafuente1@gmail.com>
2018-11-02 14:43:04 -0300

    add service config

--- webApp/src/app/sidebar/sidebar.service.ts
+++ webApp/src/app/sidebar/sidebar.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { AppService } from '@app/app.component.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SidebarService {
+  httpOptions;
+  topicsUrl;
+  constructor(private appService: AppService, private http: HttpClient) { 
+    this.httpOptions = appService.getHttpOptionsWithToken();
+    this.topicsUrl = `${appService.url}/topics`;
+  }
 
-  constructor() { }
+  errorHandler(error: HttpErrorResponse) {
+    return throwError(error.status  || 'Server Error');
+  }
+
 }
