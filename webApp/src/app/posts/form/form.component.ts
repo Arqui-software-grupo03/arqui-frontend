fe49876941e1e301e01a115ccd6d0bd33966e28a
Felipe De la Fuente V <fadelafuente1@gmail.com>
2018-11-02 02:01:56 -0300

    add flashMessages

--- webApp/src/app/posts/form/form.component.ts
+++ webApp/src/app/posts/form/form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PostsService } from '../posts.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { UsersService } from '@app/users/users.service';
 
+const TIMEOUT = 5000;
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -8,10 +11,16 @@ import { PostsService } from '../posts.service';
 })
 export class FormComponent implements OnInit {
   inputText: string;
-  constructor(private postsService: PostsService) { }
+  user;
+  constructor(private usersService: UsersService, private flashMessages: FlashMessagesService, private postsService: PostsService) { }
 
   ngOnInit() {
     this.inputText = '';
+    this.usersService.castUser.subscribe(
+      user => {
+        this.user = user;
+      }
+    );
   }
   onChangeInput(event) {
     this.inputText = event.target.value;
@@ -20,10 +29,11 @@ export class FormComponent implements OnInit {
   onSubmit(event) {
     this.postsService.createNewPost(this.inputText).subscribe(response => {
       console.log(response);
-      console.log(this.inputText);
+      this.inputText = '';
+      this.showMessage('¡Publicación creada correctamente!', 'success');
     }, error => {
       console.log(error);
-      console.log("error");
+      this.showMessage('Hubo un problema al ingresar el post, intente nuevamente.', 'danger');
     });
 
   }
@@ -32,6 +42,14 @@ export class FormComponent implements OnInit {
     if (event.keyCode === 13) {
       this.onSubmit(event);
     }
+  }
+  showMessage(message: string, type: string) {
+    this.flashMessages.show(message, {
+      cssClass: `alert-${type}`,
+      timeout: TIMEOUT,
+      showCloseBtn: true,
+      closeOnClick: true
+    });
   }
 
 }
