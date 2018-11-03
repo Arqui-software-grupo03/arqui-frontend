35cc37ade2fb1d7283b30721d690f6f08a5aee58
Felipe De la Fuente V <fadelafuente1@gmail.com>
2018-11-03 12:47:15 -0300

    add getPosts from topics

--- webApp/src/app/posts/posts.component.ts
+++ webApp/src/app/posts/posts.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { TopicService } from '@app/topic/topic.service';
 
 @Component({
   selector: 'app-posts',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.scss']
 })
 export class PostsComponent implements OnInit {
-
-  constructor() { }
+  @Input() topicId: string;
+  @Input() topicName: string;
+  postsIds;
+  constructor(private topicService: TopicService) { }
 
   ngOnInit() {
+    this.getPostsfromTopic();
   }
+
+  getPostsfromTopic() {
+    this.topicService.getAllPostFromTopic(this.topicId).subscribe(
+      postsIds => {
+        this.postsIds = postsIds;
+        console.log(postsIds);
+      }, error =>  console.log(error)
+    );
+  }
+
+
+
 
 }
