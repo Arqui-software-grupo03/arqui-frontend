101f2579f1ed2399626df66d2539e2a32411d620
Felipe De la Fuente V <fadelafuente1@gmail.com>
2018-11-03 11:40:31 -0300

    add topic module

--- webApp/src/app/sidebar/sidebar.module.ts
+++ webApp/src/app/sidebar/sidebar.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from '@app/sidebar/sidebar.component';
 import { SidebarService } from './sidebar.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ],
   declarations: [
     SidebarComponent,
