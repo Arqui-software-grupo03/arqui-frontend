cf31bb2d99bc9a763edb8136b296d6e170a25b0c
Felipe De la Fuente V <fadelafuente1@gmail.com>
2018-11-03 12:49:25 -0300

    add get topic

--- webApp/src/app/topic/topic.component.ts
+++ webApp/src/app/topic/topic.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params} from '@angular/router';
+import { TopicService } from '@app/topic/topic.service';
 
 @Component({
   selector: 'app-topic',
@@ -7,11 +8,19 @@ import { Router, ActivatedRoute, Params} from '@angular/router';
   styleUrls: ['./topic.component.scss']
 })
 export class TopicComponent implements OnInit {
-  topicName = 'General';
-  constructor(private activatedRoute: ActivatedRoute) { }
+  topicId = '0';
+  topic;
+  constructor(private activatedRoute: ActivatedRoute, private topicService: TopicService) { }
 
   ngOnInit() {
-    this.topicName = this.activatedRoute.snapshot.paramMap.get('topicName');
+    this.topicId = this.activatedRoute.snapshot.paramMap.get('topicId');
+    this.getTopic();
+  }
+  getTopic() {
+    this.topicService.getTopic(this.topicId).subscribe(
+      topic => this.topic = topic
+      , error => console.log(error)
+    );
   }
 
 }
