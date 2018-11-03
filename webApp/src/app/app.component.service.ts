e5619516122955de922fae997fedd2bed975b8ea
Felipe De la Fuente V <fadelafuente1@gmail.com>
2018-11-03 14:02:34 -0300

    add / needed in apiary

--- webApp/src/app/app.component.service.ts
+++ webApp/src/app/app.component.service.ts
@@ -11,7 +11,7 @@ export class AppService {
     };
     // Apiary: http://private-bb6ce2-arquitransocialnetwork.apiary-mock.com/
     // Apiary 'http://private-aa901-arquitransocialnetwork.apiary-mock.com'
-    url = 'http://private-aa901-arquitransocialnetwork.apiary-mock.com';
+    url = 'http://private-aa901-arquitransocialnetwork.apiary-mock.com/';
 
     private loading = new BehaviorSubject(true);
     castLoading = this.loading.asObservable();
