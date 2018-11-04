20d6a59f5a6de436988958566d64856e8e784843
Stephanie Chau <schau@uc.cl>
2018-11-04 08:55:59 -0300

    :wrench: Added default value for topic

--- webApp/src/app/topic/topic.component.ts
+++ webApp/src/app/topic/topic.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params} from '@angular/router';
 import { TopicService } from '@app/topic/topic.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-topic',
@@ -8,19 +9,35 @@ import { TopicService } from '@app/topic/topic.service';
   styleUrls: ['./topic.component.scss']
 })
 export class TopicComponent implements OnInit {
-  topicId = '0';
-  topic;
-  constructor(private activatedRoute: ActivatedRoute, private topicService: TopicService) { }
+  topicId: number;
+  topic = {};
+  constructor(private activatedRoute: ActivatedRoute, private topicService: TopicService,
+              private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
-    this.topicId = this.activatedRoute.snapshot.paramMap.get('topicId');
+    this.topicId = +this.activatedRoute.snapshot.paramMap.get('topicId');
     this.getTopic();
   }
   getTopic() {
-    this.topicService.getTopic(this.topicId).subscribe(
-      topic => this.topic = topic
-      , error => console.log(error)
+    this.topicService.getTopicById(this.topicId).subscribe(
+      topic => {
+        this.topic = topic;
+        // this.showMessage('great!', 'success');
+      },
+      error => {
+        console.log(error);
+        // this.showMessage(error, 'danger');
+      }
     );
+  }
+
+  showMessage(message: string, type: string) {
+    this.flashMessage.show(message, {
+      cssClass: `alert-${type}`,
+      timeout: 5000,
+      showCloseBtn: true,
+      closeOnClick: true
+    });
   }
 
 }
