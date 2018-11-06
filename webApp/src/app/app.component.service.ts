4a0d526b19fb76560b554e9f5c7749627e435c4d
Felipe De la Fuente V <fadelafuente1@gmail.com>
2018-11-06 15:02:03 -0300

    add charette9 http url

--- webApp/src/app/app.component.service.ts
+++ webApp/src/app/app.component.service.ts
@@ -12,6 +12,7 @@ export class AppService {
     // Apiary: http://private-bb6ce2-arquitransocialnetwork.apiary-mock.com/
     // Apiary 'http://private-aa901-arquitransocialnetwork.apiary-mock.com'
     url = 'http://private-aa901-arquitransocialnetwork.apiary-mock.com';
+    publicApiUrl = 'http://charette9.ing.puc.cl/api';
 
     private loading = new BehaviorSubject(true);
     castLoading = this.loading.asObservable();
