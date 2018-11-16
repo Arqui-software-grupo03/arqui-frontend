826d8a4965626644f77fefb07b8230e57f90fecb
Stephanie Chau <schau@uc.cl>
2018-11-15 21:23:25 -0300

    Added credential files needed

--- webApp/src/environments/environment.ts
+++ webApp/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'your apikey',
+    authDomain: 'your authDomain',
+    databaseURL: 'your databaseUrl',
+    projectId: 'your projectId',
+    storageBucket: 'your storageBucket',
+    messagingSenderId: 'your messagingSenderId'
+  }
 };
 
 /*
