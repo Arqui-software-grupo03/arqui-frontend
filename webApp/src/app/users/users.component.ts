08df33460dd90c8a6d37b1b715f0ce75b62cfca2
Stephanie Chau <schau@uc.cl>
2018-11-19 21:49:27 -0300

    Added user/:userId path and gets user for userComponent

--- webApp/src/app/users/users.component.ts
+++ webApp/src/app/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './users.service';
 import * as $ from 'jquery';
+import { ActivatedRoute, Router } from '@angular/router';
 
 declare var jQuery: any;
 
@@ -10,12 +11,36 @@ declare var jQuery: any;
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  currentUser;
   user;
-  constructor(private usersService: UsersService) { }
+  constructor(private usersService: UsersService, private route: ActivatedRoute,
+              private routerNav: Router) {
+    this.route.data.subscribe(
+      res => {
+        if (res.user) {
+          this.user = res.user;
+          // console.log(this.user);
+        } else {
+          if (this.route.snapshot.params['userId']) {
+            this.routerNav.navigate(['home']);
+          }
+        }
+    },
+    error => {
+    });
+  }
 
   ngOnInit() {
     this.usersService.castUser.subscribe(
-      usr => this.user = usr
+      usr => {
+        this.currentUser = usr;
+        if (!this.route.snapshot.params['userId']) {
+          this.user = usr;
+        }
+        if (+this.route.snapshot.params['userId'] === usr.id) {
+          this.routerNav.navigate(['profile']);
+        }
+      }
     );
     setTimeout(
       () => this.getData(), 50
